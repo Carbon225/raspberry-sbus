85b442ed400582b8401ae3a5acb3b38e670bec4e
Carbon <carbon.szlachta@gmail.com>
2020-09-01 04:34:43 +0200

    New blocking and non-blocking modes

--- src/SBUS.cpp
+++ src/SBUS.cpp
@@ -3,9 +3,9 @@
 
 #define SBUS_DESYNC_END 2
 #define SBUS_DESYNC_HDR 1
+#define READ_BUF_SIZE (SBUS_PACKET_SIZE * 2)
 
 SBUS::SBUS()
-    : _desync(SBUS_DESYNC_END)
 {}
 
 SBUS::~SBUS()
@@ -13,9 +13,9 @@ SBUS::~SBUS()
     sbus_uninstall(&_fd);
 }
 
-sbus_err_t SBUS::install(const char *path)
+sbus_err_t SBUS::install(const char *path, bool blocking)
 {
-    return sbus_install(&_fd, path);
+    return sbus_install(&_fd, path, blocking);
 }
 
 uint16_t SBUS::channel(int num) const
@@ -34,53 +34,43 @@ sbus_err_t SBUS::onPacket(sbus_packet_cb cb)
 
 sbus_err_t SBUS::read()
 {
-    int nRead = sbus_read(&_fd, _packet + _packetPos);
-    if (nRead == 1)
+    uint8_t readBuf[READ_BUF_SIZE];
+    int nRead = sbus_read(&_fd, readBuf, READ_BUF_SIZE);
+
+    bool hadDesync = false;
+
+    for (int i = 0; i < nRead; i++)
     {
-        const uint8_t newByte = _packet[_packetPos];
-        _packetPos += nRead;
-
-        if (_packetPos >= SBUS_PACKET_SIZE)
+        switch (_state)
         {
-            if (newByte == SBUS_END && _packet[0] == SBUS_HEADER)
-            {
-                uint8_t opt = 0;
-                sbus_err_t ret = sbus_decode(_packet, _channels, &opt);
-                if (ret == SBUS_OK && _packetCb != nullptr)
+            case State::WAIT_FOR_HEADER:
+                if (readBuf[i] == SBUS_HEADER)
                 {
-                    sbus_packet_t packet = {
-                            _channels,
-                            (bool) (opt & SBUS_OPT_C17),
-                            (bool) (opt & SBUS_OPT_C18),
-                            (bool) (opt & SBUS_OPT_FS),
-                            (bool) (opt & SBUS_OPT_FL)
-                    };
-                    _packetCb(packet);
+                    _packet[0] = SBUS_HEADER;
+                    _packetPos = 1;
+                    _state = State::PACKET;
                 }
-            }
-            else if (_desync == SBUS_OK)
-            {
-                fprintf(stderr, "Desync\n");
-                _desync = SBUS_DESYNC_END;
-            }
-            _packetPos = 0;
-        }
+                break;
 
-        if (_desync == SBUS_DESYNC_END && newByte == SBUS_END)
-        {
-            printf("Got packet end\n");
-            _desync = SBUS_DESYNC_HDR;
-        }
-        else if (_desync == SBUS_DESYNC_HDR && newByte == SBUS_HEADER)
-        {
-            printf("Got packet header. Desync cleared\n");;
-            _desync = SBUS_OK;
-            _packetPos = 1;
-            _packet[0] = newByte;
+            case State::PACKET:
+                _packet[_packetPos++] = readBuf[i];
+                if (_packetPos >= SBUS_PACKET_SIZE)
+                {
+                    if (verifyPacket())
+                    {
+                        decodePacket();
+                    }
+                    else
+                    {
+                        _state = State::WAIT_FOR_HEADER;
+                        hadDesync = true;
+                    }
+                }
+                break;
         }
     }
 
-    return SBUS_OK;
+    return hadDesync ? SBUS_ERR_DESYNC : SBUS_OK;
 }
 
 sbus_err_t SBUS::write(sbus_packet_t packet)
@@ -92,4 +82,26 @@ sbus_err_t SBUS::write(sbus_packet_t packet)
     opt |= packet.frameLost * SBUS_OPT_FL;
     sbus_write(&_fd, packet.channels, opt);
     return SBUS_OK;
+}
+
+bool SBUS::verifyPacket()
+{
+    return (_packet[0] == SBUS_HEADER) && (_packet[SBUS_PACKET_SIZE - 1] == SBUS_END);
+}
+
+void SBUS::decodePacket()
+{
+    uint8_t opt = 0;
+    sbus_err_t ret = sbus_decode(_packet, _channels, &opt);
+    if (ret == SBUS_OK && _packetCb != nullptr)
+    {
+        sbus_packet_t packet = {
+                _channels,
+                (bool) (opt & SBUS_OPT_C17),
+                (bool) (opt & SBUS_OPT_C18),
+                (bool) (opt & SBUS_OPT_FS),
+                (bool) (opt & SBUS_OPT_FL)
+        };
+        _packetCb(packet);
+    }
 }
