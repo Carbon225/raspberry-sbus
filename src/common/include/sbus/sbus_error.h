48198f83ed1a2b610c3228335780a759df3353f3
Carbon225 <carbon225@protonmail.com>
2021-08-05 18:14:30 +0200

    make SBUS_FAIL = -1

--- src/common/include/sbus/sbus_error.h
+++ src/common/include/sbus/sbus_error.h
@@ -4,10 +4,10 @@
 enum sbus_err_t
 {
     SBUS_OK = 0,
-    SBUS_ERR_OPEN = -1,
+    SBUS_FAIL = -1,
     SBUS_ERR_TCGETS2 = -2,
     SBUS_ERR_TCSETS2 = -3,
-    SBUS_FAIL = -4,
+    SBUS_ERR_OPEN = -4,
     SBUS_ERR_INVALID_ARG = -5,
     SBUS_ERR_DESYNC = -6,
 };
